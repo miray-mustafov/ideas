bb7dadedde681decd37fca37542737c28ea911d8
miray-mustafov <mustafovmiray@gmail.com>
2024-06-29 19:46:00 +0300

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
+/*
+MVC - Model View Controller
+Controller : Handle requests
+Controller : Handle data logic and interactions with database
+View : what is shown to the user (HTML/CSS/BladeFiles)
+*/
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/feed', function () {
+    return view('feed');
+});
+
+Route::get('/profile', function () {
+    return view('users.profile');
 });
