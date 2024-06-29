ae51ed78328358e31b91a7a691650f9c74743f98
miray-mustafov <mustafovmiray@gmail.com>
2024-06-29 22:17:18 +0300

    blade template language basics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,27 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('welcome');
+
+        $users = [
+            [
+                'name'=>'Alex',
+                'age' => 30,
+            ],
+            [
+                'name'=>'Dan',
+                'age' => 25,
+            ],
+            [
+                'name'=>'John',
+                'age' => 17,
+            ]
+
+        ];
+
+        return view(
+            'dashboard',
+        [
+            'users'=>$users
+        ]);
     }
 }
