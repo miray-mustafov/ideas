5a7d06ebe0275462fbeb71bc4496564fb22404b1
miray-mustafov <mustafovmiray@gmail.com>
2024-07-02 20:15:39 +0300

    comments-box: adding names of the users

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,6 +10,7 @@ class CommentController extends Controller
 {
     public function store(Idea $idea)
     {
+        //todo validate the content and in comment-box.blade update
         $comment = new Comment();
         $comment->idea_id = $idea->id;
         $comment->user_id = auth()->id();
