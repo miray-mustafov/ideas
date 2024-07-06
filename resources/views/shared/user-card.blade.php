4c0418ab92b4d3804d3e548b1aca56e7979ffc14
miray-mustafov <mustafovmiray@gmail.com>
2024-07-06 09:53:34 +0300

    user-edit.blade: display the follows

--- resources/views/shared/user-card.blade.php
+++ resources/views/shared/user-card.blade.php
@@ -31,7 +31,10 @@
 
             <div class="d-flex justify-content-start">
                 <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
-                                    </span> 0 hardcoded Followers </a> {{--todo followers--}}
+                                    </span> {{$user->followers()->count()}} Followers </a>
+                <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
+                                    </span> {{$user->followings()->count()}} Followings </a>
+
                 <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-brain me-1">
                                     </span> {{$user->ideas()->count()}}
                 </a> {{--SELECT COUNT(*) FROM ideas WHERE user_id = ?--}}
