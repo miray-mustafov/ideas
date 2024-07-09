f244f86b2e6c765c6ed3a4456fc6ced6d929f860
miray-mustafov <mustafovmiray@gmail.com>
2024-07-09 10:17:44 +0300

    users/scores.blade created

--- resources/views/shared/user-card.blade.php
+++ resources/views/shared/user-card.blade.php
@@ -29,18 +29,7 @@
                 {{$user->bio}}
             </p>
 
-            <div class="d-flex justify-content-start">
-                <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
-                                    </span> {{$user->followers()->count()}} Followers </a>
-                <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
-                                    </span> {{$user->followings()->count()}} Followings </a>
-
-                <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-brain me-1">
-                                    </span> {{$user->ideas()->count()}}
-                </a> {{--SELECT COUNT(*) FROM ideas WHERE user_id = ?--}}
-                <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-comment me-1">
-                                    </span> {{$user->comments()->count()}} </a>
-            </div>
+            @include('users.scores')
 
             {{--@auth() we could check for authentication within blade too but we have handled it with auth middleware --}}
             @if(auth()->id() !== $user->id)
--- resources/views/shared/user-edit-card.blade.php
+++ resources/views/shared/user-edit-card.blade.php
@@ -42,15 +42,7 @@
 
                 <button type="submit" class="btn btn-dark btn-small my-2"> Save</button>
 
-                <div class="d-flex justify-content-start">
-                    <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
-                                    </span> 0 hardcoded Followers </a> {{--todo followers--}}
-                    <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-brain me-1">
-                                    </span> {{$user->ideas()->count()}}
-                    </a> {{--SELECT COUNT(*) FROM ideas WHERE user_id = ?--}}
-                    <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-comment me-1">
-                                    </span> {{$user->comments()->count()}} </a>
-                </div>
+                @include('users.scores')
             </div>
 
         </form>
